a6a349abad97733caeb8c4de87ff96a3bfdc28b7
Hui Yang <chy1988@antwebstudio.com>
2019-10-25 17:06:31 +0800

    Updated namespace

--- src/location/widgets/views/map-input.php
+++ src/location/widgets/views/map-input.php
@@ -59,7 +59,7 @@ $enableCustomStateCountryIso = true;
 	<div v-show="isMapShow" ref="map" style="width: 100%; height: 500px;"></div>
 </div>
 
-<?php \common\widgets\JsBlock::begin() ?>
+<?php \ant\widgets\JsBlock::begin() ?>
 <script>
 var elementId = '#<?= $this->context->id ?>';
 var app = new Vue({
@@ -336,4 +336,4 @@ var app = new Vue({
 	}
 })
 </script>
-<?php \common\widgets\JsBlock::end() ?>
+<?php \ant\widgets\JsBlock::end() ?>
